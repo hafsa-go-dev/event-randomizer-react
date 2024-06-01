3ea6ecd1514528e866fcd7eb33831ce0b39b1f99
Hafsa R. <hafsarodriguez@gmail.com>
2024-06-01 03:07:48 -0400

    initial

--- src/EventGrid.js
+++ src/EventGrid.js
@@ -3,7 +3,7 @@ import {useState} from "react";
 export default function EventGrid({events, chosenEvent}) {
     let sortedEvents;
 
-    sortedEvents = events.slice().sort((a, b) => a - b);
+    sortedEvents = events.slice().sort();
 
 
 
